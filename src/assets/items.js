a666e493d4e1e39aee507d31ef93434cd109ef65
Sourabh Ahirwar <sourabha.ahirwar.ece19@itbhu.ac.in>
2022-02-20 15:19:26 +0530

    React App

---
+++ src/assets/items.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import appstore from '../assets/playstore.png'
+import youtube from '../assets/youtube.jpeg'
+import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+import telegram from '../assets/telegram.png'
+import twitter from '../assets/twitter.png'
+
+const items = [{
+    "title": "Website",
+    "subtitle": "Look at my work!",
+    "image": website,
+    "link": "http://google.com" //your personal website or portfolio  link
+},
+{
+    "title": "GitHub",
+    "subtitle": "Projects",
+    "image": github,
+    "link": "https://github.com" //Github Profile link
+},
+{
+    "title": "Instagram",
+    "subtitle": "sou.r_ab17",
+    "image": instagram,
+    "link": "https://instagram.com/sou.r_ab17" //instagram profile link 
+},
+{
+    "title": "Twitter",
+    "subtitle": "Lorem ipsum",
+    "image": twitter,
+    "link": "https://twitter.com"// twitter profile link 
+},
+
+
+
+{
+    "title": "Leetocde",
+    "subtitle": "Coding Platform ",
+    "image": youtube,
+    "link": "https://leetcode.com/sourabh_29/" // Dribbble profile link 
+},
+{
+    "title": "Facebook",
+    "subtitle": "Social Media",
+    "image": appstore,
+    // "image": icons8-facebook-256,
+    "link": "https://facebook.com"//
+},
+{
+    "title": "YouTube",
+    "subtitle": "Coding and entertaiinment stuff",
+    "image": youtube,
+    "link": "https://www.youtube.com"//youtube channel link 
+},
+{
+    "title": "Telegram",
+    "subtitle": "Chat groups and movie download ",
+    "image": telegram,
+    "link": "https://telegram.me" //Telegram Pofile 
+}]
+
+export default items
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../css/skeleton.css'
+import '../css/normalize.css'
+import '../css/components.css'
+
+function Footer() {
+    return (
+        <div className="Footer container">
+            <p>URL AGGREGATOR<span class="heart"></span> <a href={"https://twitter.com/heysagnik"}>WEBSITE</a></p>
+        </div>
+    )
+}
+
+export default Footer
